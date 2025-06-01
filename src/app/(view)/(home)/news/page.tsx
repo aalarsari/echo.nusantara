9e972ae1967956de3b7c62f0ae11addad0d8b560
boxxy <hanifakbri7@gmail.com>
2025-06-01 16:35:47 +0700

    update

--- src/app/(view)/(home)/news/page.tsx
+++ src/app/(view)/(home)/news/page.tsx
@@ -59,51 +59,24 @@ export default function News() {
         <div className="flex w-full h-screen items-center flex-col lg:flex-row gap-2 justify-center bg-[#F4F4F4] lg:py-10 lg:px-10">
           <div className="w-full h-[656px]">
             {news.lastNews ? (
-              <div
-                onClick={() => handleLastNewsClick(news.lastNews.slug)}
-                className="w-full h-full"
-              >
+              <div className="w-full h-full">
                 <div className="flex h-full w-full justify-center overflow-hidden lg:rounded-[10px] relative">
-                  {/* <Image
-                    src={news.lastNews.image || Assets.DefaultImage}
-                    alt="Last News"
-                    fill
-                    priority
-                    style={{ objectFit: "contain" }}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                  /> */}
                   <div className="absolute inset-0 bg-gradient-to-t from-black to-black/0 z-[10]" />
                   <div className="absolute inset-0 z-20 py-10 px-4 ">
                     <div className="flex flex-col justify-end items-end relative gap-8 h-full w-full px-4">
-                      <div className="w-full">
-                        <div className="flex flex-col gap-1">
-                          <span className="font-domaine uppercase text-[14px] font-light text-white text-left">
-                            {news.lastNews.category ?? "No Category"}
-                          </span>
-                          <h2 className="font-domaine text-[24px] font-semibold text-white text-left">
-                            {news.lastNews.title ?? "Title"}
-                          </h2>
-                        </div>
-                        <span className="font-domaine text-[16px] text-white text-left">
-                          {news.lastNews.subtitle ?? "Subtitle"}
-                        </span>
-                      </div>
                       <div className="w-full flex justify-between items-center">
                         <div className="flex flex-row gap-2 justify-center items-center">
-                          <Image
-                            src={Assets.TimeWhite || Assets.DefaultImage}
-                            alt="Time"
-                            width={16}
-                            height={16}
-                          />
-                          {/* <span className="font-domaine text-[14px] font-light text-white">
-                            {moment(news.lastNews.createdAt).format(
-                              "DD MMMM YYYY"
-                            )}
-                          </span> */}
+                          {news.lastNews?.createdAt && (
+                            <span className="font-domaine text-[14px] font-light text-white">
+                              {moment(news.lastNews.createdAt).format(
+                                "DD MMMM YYYY"
+                              )}
+                            </span>
+                          )}
                         </div>
                         <button
                           onClick={() =>
+                            news.lastNews?.slug &&
                             handleLastNewsClick(news.lastNews.slug)
                           }
                           className="font-domaine text-[14px] rounded-full px-4 py-1 font-semibold bg-white text-[#B69B7C] ring-1 ring-[#7D716A]"
