904520730b55e1798a4553002e45bfdda2ae3d51
boxxy <hanifakbri7@gmail.com>
2025-06-01 16:09:44 +0700

    update

--- src/app/(view)/(home)/news/page.tsx
+++ src/app/(view)/(home)/news/page.tsx
@@ -10,13 +10,9 @@ import { useRouter } from "next/navigation";
 
 export default function News() {
   const [news, setNewsData] = useState<{
-    blog: blog[];
     lastNews: any | null;
-    topBlog: blog[];
   }>({
-    blog: [],
     lastNews: null,
-    topBlog: [],
   });
   const [pageSize, setPageSize] = useState<number>(6);
   const [page, setPage] = useState<number>(1);
@@ -48,14 +44,6 @@ export default function News() {
     }
   };
 
-  const handleBlogClick = (slug: string) => {
-    try {
-      router.push(`/news/article/${slug}`);
-    } catch (error) {
-      console.error("Error navigating to product detail:", error);
-    }
-  };
-
   return (
     <div className="relative h-full w-full">
       <div className="w-full flex items-center justify-center py-16 lg:py-24">
@@ -71,11 +59,7 @@ export default function News() {
                 onClick={() => handleLastNewsClick(news.lastNews.slug)}
                 className="w-full h-full"
               >
-                <div
-                  style={{ position: "relative" }}
-                  className="flex h-full w-full justify-center overflow-hidden lg:rounded-[10px]"
-                >
-                  {/* Gambar */}
+                <div className="flex h-full w-full justify-center overflow-hidden lg:rounded-[10px] relative">
                   <Image
                     src={news.lastNews.image[0] || Assets.DefaultImage}
                     alt="Last News"
@@ -93,17 +77,17 @@ export default function News() {
                             {news.lastNews.category ?? "No Category"}
                           </span>
                           <h2 className="font-domaine text-[24px] font-semibold text-white text-left">
-                            {news.lastNews.title}
+                            {news.lastNews.title ?? "Title"}
                           </h2>
                         </div>
                         <span className="font-domaine text-[16px] text-white text-left">
-                          {news.lastNews.subtitle}
+                          {news.lastNews.subtitle ?? "Subtitle"}
                         </span>
                       </div>
                       <div className="w-full flex justify-between items-center">
                         <div className="flex flex-row gap-2 justify-center items-center">
                           <Image
-                            src={Assets.TimeWhite}
+                            src={Assets.TimeWhite || Assets.DefaultImage}
                             alt="Time"
                             width={16}
                             height={16}
@@ -131,73 +115,6 @@ export default function News() {
               <div className="w-full h-full flex items-center justify-center bg-gray-100 lg:rounded-[10px] animate-pulse">
                 <span className="text-gray-500 text-sm">
                   Loading latest news...
-                </span>
-              </div>
-            )}
-          </div>
-
-          <div className="relative w-full h-[656px] overflow-y-scroll">
-            {news.topBlog && news.topBlog.length > 0 ? (
-              news.topBlog.map((item, index) => (
-                <div
-                  key={index}
-                  onClick={() => handleBlogClick(item.slug)}
-                  className="flex flex-col gap-4 py-2 px-4"
-                >
-                  <div className="h-[225px] relative overflow-hidden bg-white w-full cursor-pointer shadow-product rounded-[16px] shadow-gray-100">
-                    <div className="flex h-full w-full flex-row gap-2">
-                      <div className="relative h-full w-[225px]">
-                        <Image
-                          src={item.image[0] || Assets.DefaultImage}
-                          fill
-                          style={{ objectFit: "cover" }}
-                          priority={true}
-                          alt={`Top Blog ${index + 1}`}
-                          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                        />
-                      </div>
-                      <div className="flex flex-col justify-center items-start gap-8 h-full w-[60%] px-4 relative overflow-hidden">
-                        <div className="w-full">
-                          <div className="flex flex-col gap-1">
-                            <span className="font-domaine uppercase text-[14px] font-light text-black">
-                              {item.category?.name ?? "No Category"}
-                            </span>
-                            <h2 className="font-domaine text-[24px] font-semibold text-black truncate w-full">
-                              {item.title}
-                            </h2>
-                          </div>
-                          <h2 className="font-domaine text-[16px] font-normal text-black truncate w-[300px]">
-                            {item.subtitle}
-                          </h2>
-                        </div>
-                        <div className="w-full flex justify-between items-center">
-                          <div className="flex flex-row gap-2 items-center justify-center">
-                            <Image
-                              src={Assets.TimeBronze}
-                              alt="Last News"
-                              width={16}
-                              height={16}
-                            />
-                            <span className="font-domaine text-[14px] font-light text-black">
-                              {moment(item.updateAt).format("DD MMMM YYYY")}
-                            </span>
-                          </div>
-                          <button
-                            onClick={() => handleBlogClick(item.slug)}
-                            className="font-domaine text-[14px] rounded-full px-4 py-1 font-semibold text-[#B69B7C] ring-1 ring-[#7D716A]"
-                          >
-                            Read More
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div className="w-full h-full flex items-center justify-center bg-gray-100 lg:rounded-[10px] animate-pulse">
-                <span className="text-gray-500 text-sm">
-                  Loading News . . .
                 </span>
               </div>
             )}
